19a016e057c5ca0d0dc02645d00e2a1a719e8edd
joelmanrique91-lgtm <joelmanrique91@gmail.com>
2026-02-07 01:44:13 -0300

    Add reporting run directories and manifests

---
+++ src/geostats_pipeline/__init__.py
@@ -0,0 +1,5 @@
+"""Geostats pipeline helpers."""
+
+from .reporting import RunPaths, create_run_dir, save_figure, save_table, write_manifest
+
+__all__ = ["RunPaths", "create_run_dir", "save_figure", "save_table", "write_manifest"]

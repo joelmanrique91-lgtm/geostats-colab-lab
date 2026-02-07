31c591301f2c64d3a957f80e016ce5192a54bed6
joelmanrique91-lgtm <joelmanrique91@gmail.com>
2026-02-07 01:37:43 -0300

    Add compositing and declustering modules

---
+++ src/geostats_pipeline/__init__.py
@@ -0,0 +1,4 @@
+"""Utilidades para el pipeline geoestadístico."""
+
+from .compositing import composite_by_length, load_config  # noqa: F401
+from .declustering import cell_declustering  # noqa: F401
